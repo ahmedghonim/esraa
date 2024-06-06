343c463bde9f612cfec762fb91c4e5bf81fa1170
Fahiim <ahmedfahiim01@gmail.com>
2024-06-06 19:57:12 +0300

    add add to cart function

--- views/shopper/local-cart/index.tsx
+++ views/shopper/local-cart/index.tsx
@@ -55,8 +55,6 @@ export default function LocalCart({ children }: Props) {
         (total, item) => total + item.qty * item.price,
         0
       );
-
-      // setCart({ ...cart, subTotal, total: subTotal - cart.shipping });
     }
   }, [cart]);
 
