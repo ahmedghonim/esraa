a9df289ddc168473c1983778e280b2deea667ba1
Ahmed Ghonim <35805998+ahmedghonim@users.noreply.github.com>
2024-06-06 17:50:43 +0300

    chore: Update metadata generation for multilingual support
    Refactor the generateMetadata function in the layout.tsx file to include multilingual support. The function now generates metadata based on the selected language, allowing for proper localization of titles, URLs, and alternate language options. This improves the SEO and user experience by providing accurate metadata for each language version of the website.

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -3,12 +3,83 @@ import { El_Messiri } from "next/font/google";
 import Layout from "@/layout";
 import LocalCart from "@/views/shopper/local-cart";
 import { Toaster } from "@/components/ui/toaster";
+import { getTranslations } from "next-intl/server";
+import { Metadata } from "next";
 
 const cairo_font = El_Messiri({
   subsets: ["arabic", "latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-cairo",
 });
+export async function generateMetadata(
+  {
+    params: { lang },
+  }: {
+    params: { lang: string };
+  },
+  cookieLang: string | undefined
+): Promise<Metadata> {
+  return {
+    title: {
+      template: `Esra | %s`,
+      default: "Esra",
+    },
+    // description: t("description"),
+    // keywords: t("keywords"),
+    authors: [{ name: "esramodestwear" }],
+    applicationName: "esramodestwear",
+    metadataBase: new URL("https://www.esramodestwear.com"),
+    alternates: {
+      canonical: `${cookieLang ? "/" + (lang === "en" ? "" : `${lang}`) : ""}`,
+      languages: {
+        en: "/",
+        "en-US": "/",
+        "en-au": "/",
+        "en-bz": "/",
+        "en-ca": "/",
+        "en-ie": "/",
+        "en-jm": "/",
+        "en-nz": "/",
+        "en-za": "/",
+        "en-tt": "/",
+        "en-gb": "/",
+        "en-us": "/",
+        "ar-AR": "/ar",
+        "ar-dz": "/ar",
+        "ar-bh": "/ar",
+        "ar-eg": "/ar",
+        "ar-iq": "/ar",
+        "ar-jo": "/ar",
+        "ar-kw": "/ar",
+        "ar-lb": "/ar",
+        "ar-ly": "/ar",
+        "ar-ma": "/ar",
+        "ar-om": "/ar",
+        "ar-qa": "/ar",
+        "ar-sa": "/ar",
+        "ar-sy": "/ar",
+        "ar-tn": "/ar",
+        "ar-ae": "/ar",
+        "ar-ye": "/ar",
+      },
+    },
+
+    openGraph: {
+      type: "website",
+      title: "Esra Modestwear",
+      url: `https://www.esramodestwear.com/${lang}`,
+      siteName: "esramodestwear",
+      images: [
+        {
+          url: "./logo.jpg",
+          width: 800,
+          height: 600,
+          alt: "esramodestwear",
+        },
+      ],
+    },
+  };
+}
 
 export default function RootLayout({
   children,
