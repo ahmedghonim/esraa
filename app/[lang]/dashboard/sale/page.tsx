4ff1166abe94329861ed8e2cf3cb9a7d140d9c56
Ahmed Ghonim <35805998+ahmedghonim@users.noreply.github.com>
2024-06-06 13:52:54 +0300

    const data = (await getAllSaleSliders()) as a

--- app/[lang]/dashboard/sale/page.tsx
+++ app/[lang]/dashboard/sale/page.tsx
@@ -1,10 +1,9 @@
-import { getAllProducts } from "@/actions/product";
 import { getAllSaleSliders } from "@/actions/slae";
 import SaleList from "@/views/admin/sale";
 import React from "react";
 
 export default async function ProductsPage() {
-  const data = (await getAllSaleSliders()) as any;
+  const data = (await getAllSaleSliders({})) as any;
 
   return <SaleList data={data} />;
 }
