296f357a116f5988db858659bcb7ebfbb96ace86
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2024-11-24 18:49:58 +0200

    Update IntroSection to use specific translations and adjust layout spacing

--- views/shopper/home/intro-section/index.tsx
+++ views/shopper/home/intro-section/index.tsx
@@ -4,7 +4,7 @@ import { getTranslations } from "next-intl/server";
 import { Button } from "@/components/ui/button";
 import { Link } from "@/utils/navigation";
 export default async function IntroSection() {
-  const t = await getTranslations();
+  const t = await getTranslations("common");
   const data = (await getHeroSection()) as any;
 
   return (
@@ -16,7 +16,7 @@ export default async function IntroSection() {
         className="w-full h-screen absolute end-[0px] start-[0px] md:top-0 -top-8 -z-10 object-cover"
         src={data?.title}
       />
-      <div className="flex flex-col items-center mt-[10%] h-full w-full relative z-10">
+      <div className="flex flex-col items-center mt-[20%] h-full w-full relative z-10">
         <h1 className="text-5xl text-white font-bold text-center">
           {parser(data?.description)}
         </h1>
