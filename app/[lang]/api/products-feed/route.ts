4f540a3ed5402013ea669a63c435d5553fa3a2c1
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2025-05-20 23:15:39 +0300

    Add XML product feed API endpoint and update layout structure for improved rendering.

--- app/[lang]/api/products-feed/route.ts
+++ app/[lang]/api/products-feed/route.ts
@@ -3,7 +3,12 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const products = await prisma.product.findMany();
+  const products = await prisma.product.findMany({
+    include: {
+      ProductVariant: true,
+    },
+  });
+  console.log("🚀 ~ :11 ~ GET ~ products:", products);
   const xml = `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:g="http://base.google.com/ns/1.0">
   <channel>
@@ -11,20 +16,31 @@ export async function GET() {
     <link>https://esramodestwear.com</link>
     <description>Product Feed</description>
     ${products
-      .map(
-        (product) => `
+      .map((product) => {
+        const description = product.description
+          ? product.description
+              .replace(/<[^>]*>?/gm, "")
+              .replace(/&nbsp;/g, " ")
+              .replace(/&/g, "&amp;")
+          : "";
+        return `
       <item>
         <g:id>${product.id}</g:id>
-        <g:title>${product.name}</g:title>
-        <g:description>${product.description}</g:description>
+        <g:title>${product.name
+          .replace(/<[^>]*>?/gm, "")
+          .replace(/&nbsp;/g, " ")
+          .replace(/&/g, "&amp;")}</g:title>
+        <g:description>${description}</g:description>
         <g:link>https://esramodestwear.com/products/${product.slug}</g:link>
         <g:image_link>${product.images[0]}</g:image_link>
         <g:price>${product.price} USD</g:price>
-        <g:availability>in stock</g:availability>
+        <g:availability>${
+          product.ProductVariant.length > 0 ? "in stock" : "out of stock"
+        }</g:availability>
         <g:condition>new</g:condition>
       </item>
-    `
-      )
+    `;
+      })
       .join("")}
   </channel>
 </rss>`;
